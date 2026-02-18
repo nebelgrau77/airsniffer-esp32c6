9d5260c826d3c465ffae0ee0eb4fe1cb2919c400
nebelgrau77 <misiek@gmx.it>
2026-02-18 21:48:48 +0100

    works with esp-hal from git repo, correctly displays layout and updating number, uses local mipidsi repo with corrected resolution

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,8 +10,10 @@
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
+use embedded_graphics::mock_display::ColorMapping;
 use esp_hal::clock::CpuClock;
 use esp_hal::timer::timg::TimerGroup;
+use esp_println as _;
 
 use esp_hal::{
   delay::Delay,
@@ -30,6 +32,10 @@ use esp_hal::{
   },  
 };
 
+
+
+use esp_hal::i2c::master::{I2c, Config as I2cConfig};
+
 use core::fmt::Write;   
 use arrayvec::ArrayString;
 
@@ -42,7 +48,7 @@ use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
 
 // Larger font
-use profont::{PROFONT_18_POINT, PROFONT_24_POINT};
+use profont::{PROFONT_10_POINT, PROFONT_14_POINT, PROFONT_18_POINT, PROFONT_24_POINT};
 
 // TFT Screen stuff
 use mipidsi::{
@@ -54,6 +60,13 @@ use mipidsi::{
         Orientation, Rotation
     }
 };
+
+// For ratatui
+use mousefood::{EmbeddedBackend, EmbeddedBackendConfig, fonts};
+use ratatui::{layout::{Constraint, Flex, Layout}};
+use ratatui::widgets::{Block, Paragraph, Wrap, Gauge};
+use ratatui::{style::*, Frame, Terminal};
+
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -81,7 +94,7 @@ esp_bootloader_esp_idf::esp_app_desc!();
 async fn main(spawner: Spawner) -> ! {
     // generator version: 1.2.0
 
-    rtt_target::rtt_init_defmt!();
+    //rtt_target::rtt_init_defmt!();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
@@ -96,25 +109,32 @@ async fn main(spawner: Spawner) -> ! {
     info!("Embassy initialized!");
 
 
+    /*
+    let i2c_bus = I2c::new(
+        peripherals.I2C0,
+        I2cConfig::default().with_frequency(Rate::from_khz(100)),
+    )
+    .unwrap()
+    .with_scl(peripherals.GPIO23)
+    .with_sda(peripherals.GPIO22);
+
+    info!("I2C initialized!");
+
+ */
+
       let spi = Spi::new(
     peripherals.SPI2,
     SpiConfig::default()
-        .with_frequency(Rate::from_mhz(60))
+        .with_frequency(Rate::from_mhz(25))
         .with_mode(SpiMode::_0))
         .unwrap()
         .with_sck(peripherals.GPIO19)
         .with_mosi(peripherals.GPIO18);
 
-    /*
-    let cs = Output::new(peripherals.GPIO4, Level::Low, OutputConfig::default());
-    let dc = Output::new(peripherals.GPIO2, Level::Low, OutputConfig::default());
-     */
     let cs = Output::new(peripherals.GPIO1, Level::Low, OutputConfig::default());
     let dc = Output::new(peripherals.GPIO21, Level::Low, OutputConfig::default());
 
     let reset = Output::new(peripherals.GPIO2, Level::Low, OutputConfig::default());
-
-
     let buffer = SPI_BUFFER.init([0; 1024]);
 
     let spi_dev = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
@@ -137,18 +157,22 @@ async fn main(spawner: Spawner) -> ! {
     display.clear(Rgb565::BLACK).unwrap();
 
 
-    let text_style = MonoTextStyle::new(&PROFONT_24_POINT, Rgb565::RED);
-    Text::with_baseline("impl Rust", Point::new(10, 20), text_style, Baseline::Top)
+    const TEAL: Rgb565 = Rgb565::new(100, 220, 220); //Color = Color::Rgb(100, 220, 220);
+    const ORANGE: Rgb565 = Rgb565::new(250, 145, 55);
+
+
+    let text_style = MonoTextStyle::new(&PROFONT_24_POINT, TEAL);
+    Text::with_baseline("test", Point::new(30, 10), text_style, Baseline::Top)
     .draw(&mut display)
     .unwrap();
 
-    let text_style = MonoTextStyle::new(&PROFONT_18_POINT, Rgb565::GREEN);
+    let text_style = MonoTextStyle::new(&PROFONT_14_POINT, ORANGE);
 
-    Text::with_baseline("for ESP32", Point::new(30, 60), text_style, Baseline::Top)
+    Text::with_baseline("of TFT", Point::new(20, 50), text_style, Baseline::Top)
         .draw(&mut display)
         .unwrap();
 
-    Timer::after(Duration::from_millis(5000)).await;
+    Timer::after(Duration::from_secs(5)).await;
 
     display.clear(Rgb565::WHITE).unwrap();
 
@@ -156,13 +180,174 @@ async fn main(spawner: Spawner) -> ! {
 
     display.clear(Rgb565::RED).unwrap();
 
+    Timer::after(Duration::from_millis(500)).await;
+
+    display.clear(Rgb565::BLACK).unwrap();
+
+    Timer::after(Duration::from_secs(2)).await;
+
     // TODO: Spawn some tasks
     let _ = spawner;
+
+    
+    // Create a custom config with a flush callback
+    let backend_config = EmbeddedBackendConfig 
+    {
+        font_regular: fonts::MONO_6X12_OPTIMIZED,
+        
+        ..Default::default()        
+    };
+
+
+    let backend = EmbeddedBackend::new(&mut display, backend_config);
+
+    let mut terminal = Terminal::new(backend).unwrap();
+
+      info!("mousefood set up");
+
+    //terminal.draw(draw).unwrap();
+
+    /*
+    terminal.draw(
+        |frame| {
+                draw(frame);
+        }).unwrap();    
+    */
+
+    let mut val: u16 = 0;
 
     loop {
         info!("Hello world!");
         Timer::after(Duration::from_secs(1)).await;
+        
+        terminal.draw(
+        |frame| {
+                draw(frame, val);
+        }).unwrap();    
+
+        val += 1;
+    
+  
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
+}
+
+
+
+
+ const TEAL: Color = Color::Rgb(100, 220, 220);
+const ORANGE: Color = Color::Rgb(250, 145, 55);
+
+fn draw(frame: &mut Frame, value: u16) {
+        
+    let vertical = Layout::vertical([
+        //Constraint::Percentage(30),         
+        Constraint::Percentage(30), 
+        Constraint::Percentage(30),
+        Constraint::Percentage(30), 
+        ]).flex(Flex::Center);
+    let [//first,         
+        second, 
+         third ,
+         fourth,         
+        ] = vertical.areas(frame.area());
+
+    let horizontal_third = Layout::horizontal([Constraint::Percentage(50), Constraint::Percentage(50)]);
+    let [third_bottom_left, third_bottom_right] = horizontal_third.areas(third);
+    let horizontal_fourth = Layout::horizontal([Constraint::Percentage(50), Constraint::Percentage(50)]);
+    let [fourth_bottom_left, fourth_bottom_right] = horizontal_fourth.areas(fourth);
+    
+    
+    let gauge = Gauge::default()            
+            .gauge_style(Style::new().fg(TEAL).bg(Color::Black))
+            .ratio(1_f64)
+            .label("excellent");
+        
+
+    let bordered_block = Block::bordered()
+        .border_style(Style::new().fg(TEAL))                
+        .title("Air Quality");
+    
+    frame.render_widget(gauge.block(bordered_block), second);
+     
+
+    // four frames - top left        
+
+    let mut textbuffer = ArrayString::<16>::new();
+    write!(&mut textbuffer, "{} C", round_float(23.5)).unwrap();
+
+    //let paragraph = Paragraph::new(textbuffer.as_str().white())
+    let paragraph = Paragraph::new(textbuffer.as_str().fg(ORANGE))
+        .wrap(Wrap { trim: true })
+        .centered();
+
+    let bordered_block = Block::bordered()
+        .border_style(Style::new().fg(TEAL))
+        //.padding(Padding::new(0, 0, third_bottom_left.height / 4, 0))
+        .title("Temperature");
+    
+    frame.render_widget(paragraph.block(bordered_block), third_bottom_left);
+
+    // four frames - top right        
+
+    let mut textbuffer = ArrayString::<16>::new();
+    write!(&mut textbuffer, "{} %", round_float(65.3)).unwrap();
+
+    let paragraph = Paragraph::new(textbuffer.as_str().fg(ORANGE))
+        .wrap(Wrap { trim: true })
+        .centered()
+        ;
+
+    let bordered_block = Block::bordered()
+        .border_style(Style::new().fg(TEAL))
+        //.padding(Padding::new(0, 0, third_bottom_right.height / 4, 0))
+        .title("Humidity");
+
+    frame.render_widget(paragraph.block(bordered_block), third_bottom_right);
+
+
+    // four frames - bottom left        
+
+    let mut textbuffer = ArrayString::<16>::new();
+    write!(&mut textbuffer, "{} hPa", round_float(985.2)).unwrap();
+
+
+    let paragraph = Paragraph::new(textbuffer.as_str().fg(ORANGE))
+        .wrap(Wrap { trim: true })
+        .centered()
+        ;
+
+    let bordered_block = Block::bordered()
+        .border_style(Style::new().fg(TEAL))
+        //.padding(Padding::new(0, 0, fourth_bottom_left.height / 4, 0))
+        .title("Pressure");
+
+    frame.render_widget(paragraph.block(bordered_block), fourth_bottom_left);
+
+    
+    // four frames - bottom right
+
+    let mut textbuffer = ArrayString::<16>::new();
+    write!(&mut textbuffer, "{}", value).unwrap();
+
+    
+
+    let paragraph = Paragraph::new(textbuffer.as_str().fg(ORANGE))
+        .wrap(Wrap { trim: true })
+        .centered()
+        ;
+
+    let bordered_block = Block::bordered()
+        .border_style(Style::new().fg(TEAL))
+        //.padding(Padding::new(0, 0, fourth_bottom_right.height / 4, 0))
+        .title("TVOC");
+
+    frame.render_widget(paragraph.block(bordered_block), fourth_bottom_right);
+
+}
+
+
+fn round_float(val: f32) -> f32 {
+    (((val * 100_f32) as i32) as f32) / 100_f32     
 }
