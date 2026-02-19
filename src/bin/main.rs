6a392eaf430ff2663bf762f4e5176fb29c23bdfd
nebelgrau77 <misiek@gmx.it>
2026-02-19 20:00:41 +0100

    works correctly displaying BME and ENS data

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,40 +8,32 @@
 #![deny(clippy::large_stack_frames)]
 
 use defmt::info;
+use esp_println as _;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer, Delay as DelayNs};
-use embedded_graphics::mock_display::ColorMapping;
-use esp_hal::clock::CpuClock;
-use esp_hal::timer::timg::TimerGroup;
-use esp_println as _;
 
 use esp_hal::{
-  delay::Delay,
-  spi::{
-    master::{
-      Config as SpiConfig,
-      Spi
+    clock::CpuClock, 
+    timer::timg::TimerGroup,
+    delay::Delay,
+    spi::{
+        master::{
+        Config as SpiConfig,
+        Spi
+        },
+        Mode as SpiMode,
     },
-    Mode as SpiMode,
-  },
-  time::Rate,
-  gpio::{
-    Level,
-    Output,
-    OutputConfig
-  },  
-  Async
+    i2c::master::
+        {I2c, Config as I2cConfig
+        },
+    time::Rate,
+    gpio::{
+        Level,
+        Output,
+        OutputConfig
+    },  
+    Async
 };
-
-
-
-use esp_hal::i2c::master::{I2c, Config as I2cConfig};
-
-use core::fmt::Write;   
-use arrayvec::ArrayString;
-
-use static_cell::StaticCell;
-use embedded_hal_bus::spi::ExclusiveDevice;
 
 use embassy_sync::{
     signal::Signal,
@@ -53,12 +45,9 @@ use embassy_sync::{
     pubsub::PubSubChannel,    
 };
 
-// Embedded graphics stuff
-use embedded_graphics::pixelcolor::Rgb565;
-use embedded_graphics::prelude::*;
-
-// Larger font
-use profont::{PROFONT_10_POINT, PROFONT_14_POINT, PROFONT_18_POINT, PROFONT_24_POINT};
+use static_cell::StaticCell;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
 
 // TFT Screen stuff
 use mipidsi::{
@@ -71,25 +60,38 @@ use mipidsi::{
     }
 };
 
+// Embedded graphics stuff
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::*;
+use embedded_graphics::mono_font::MonoTextStyle;
+use embedded_graphics::text::{Baseline, Text};
+
+// Larger font
+use profont::{PROFONT_10_POINT};
+
+use core::fmt::Write;   
+use arrayvec::ArrayString;
+
 // For ratatui
 use mousefood::{EmbeddedBackend, EmbeddedBackendConfig, fonts};
-use ratatui::{layout::{Constraint, Flex, Layout}};
+use ratatui::{layout::{Constraint, Flex, Layout}, style::palette::material::GRAY};
 use ratatui::widgets::{Block, Paragraph, Wrap, Gauge};
 use ratatui::{style::*, Frame, Terminal};
 
-use embassy_embedded_hal::shared_bus::asynch::i2c::I2cDevice;
+// sensors
 use ens160::{Ens160};
+use bme280_rs::{AsyncBme280, Configuration, Oversampling, SensorMode};
 
-use bme280_rs::{Bme280, AsyncBme280, Configuration, Oversampling, SensorMode};
-
-// Types defined for I2C devices (bus, display)
+// Types defined for I2C devices (bus)
 type SharedI2cDevice = I2cDevice<'static, NoopRawMutex, I2c<'static, Async>>;
+
+// SPI buffer for the display
+static SPI_BUFFER: StaticCell<[u8; 1024]> = StaticCell::new();
 
 // I2C shared bus
 static I2CBUS: StaticCell<Mutex<NoopRawMutex, I2c<'static, Async>>> = StaticCell::new();
 
-// Signal to pass HH:MM:SS time value between tasks
-//static TIMESIGNAL: Signal<CriticalSectionRawMutex, DateTime> = Signal::new();
+// signals to pass between tasks
 static AQISIGNAL: Signal<CriticalSectionRawMutex, AirQualityData> = Signal::new();
 
 // Signal to pass BME data between tasks
@@ -98,6 +100,7 @@ static ENVIROSIGNAL: PubSubChannel<CriticalSectionRawMutex, Enviro, 1,3, 1> = Pu
 // BME280 sensor
 static BME280_CELL: StaticCell<AsyncBme280<SharedI2cDevice, DelayNs>> = StaticCell::new();
 
+// structs to hold sensor data
 #[derive(Clone, Copy)]
 struct AirQualityData {
     temperature: f32,    
@@ -106,7 +109,6 @@ struct AirQualityData {
     tvoc: u16,    
     aqi: u8
     }
-
 
 #[derive(Clone, Copy)]
 struct Enviro {
@@ -123,12 +125,6 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 
 extern crate alloc;
 
-// Embedded Grpahics related
-use embedded_graphics::mono_font::MonoTextStyle;
-use embedded_graphics::prelude::*;
-use embedded_graphics::text::{Baseline, Text};
-
-static SPI_BUFFER: StaticCell<[u8; 1024]> = StaticCell::new();
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -155,8 +151,6 @@ async fn main(spawner: Spawner) -> ! {
     esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
 
     info!("Embassy initialized!");
-
-
     
     let i2c_bus = I2c::new(
         peripherals.I2C0,
@@ -170,28 +164,26 @@ async fn main(spawner: Spawner) -> ! {
     info!("I2C initialized!");
 
     let bus = Mutex::<NoopRawMutex, _>::new(i2c_bus);
-    //let bus = Mutex::<NoopRawMutex, _>::new(core::cell::RefCell::new(i2c_bus));  
     let bus = I2CBUS.init(bus);
-
 
     info!("shared I2C bus set up");
 
     let mut ens160_aqi = Ens160::new(I2cDevice::new(bus), 0x53);
     info!("Initialized ENS160");
-    Timer::after(Duration::from_millis(250)).await;
+
+    Timer::after(Duration::from_millis(10)).await;
     
     ens160_aqi.reset().await.ok();
     info!("ENS160 reset");
-    Timer::after(Duration::from_millis(250)).await;
+    Timer::after(Duration::from_millis(10)).await;
     
     ens160_aqi.operational().await.ok();
 
     info!("ens 160 id: {}", ens160_aqi.part_id().await.unwrap());
 
-
     let delayns = DelayNs {};
 
-    let mut bme280 = AsyncBme280::new(I2cDevice::new(bus), delayns);
+    let bme280 = AsyncBme280::new(I2cDevice::new(bus), delayns);
 
     let bme280 = BME280_CELL.init(bme280);
 
@@ -205,7 +197,9 @@ async fn main(spawner: Spawner) -> ! {
         .with_sensor_mode(SensorMode::Normal)
     ).await.unwrap();
 
+    info!("BME280 set up");
     
+    info!("bme280 id: {}", bme280.chip_id().await.unwrap());
 
     Timer::after(Duration::from_millis(10)).await;
 
@@ -216,7 +210,9 @@ async fn main(spawner: Spawner) -> ! {
 }
  
 
-      let spi = Spi::new(
+    // set up display
+
+    let spi = Spi::new(
     peripherals.SPI2,
     SpiConfig::default()
         .with_frequency(Rate::from_mhz(25))
@@ -251,82 +247,65 @@ async fn main(spawner: Spawner) -> ! {
     display.clear(Rgb565::BLACK).unwrap();
 
 
-    const TEAL: Rgb565 = Rgb565::new(100, 220, 220); //Color = Color::Rgb(100, 220, 220);
-    const ORANGE: Rgb565 = Rgb565::new(250, 145, 55);
-
-
-    let text_style = MonoTextStyle::new(&PROFONT_10_POINT, TEAL);
+    let text_style = MonoTextStyle::new(&PROFONT_10_POINT, Rgb565::CSS_GREEN_YELLOW);
     Text::with_baseline("system starting...", Point::new(10, 10), text_style, Baseline::Top)
     .draw(&mut display)
     .unwrap();
     
     Timer::after(Duration::from_secs(2)).await;
 
-    /*
-    display.clear(Rgb565::WHITE).unwrap();
-
-    Timer::after(Duration::from_millis(500)).await;
-
-    display.clear(Rgb565::RED).unwrap();
-
-    Timer::after(Duration::from_millis(500)).await;
-    */
+    
     display.clear(Rgb565::BLACK).unwrap();
 
     Timer::after(Duration::from_millis(500)).await;
-    //Timer::after(Duration::from_secs(2)).await;
-
-
+    
     let led = Output::new(peripherals.GPIO15, Level::Low, OutputConfig::default());
 
     // TODO: Spawn some tasks
     spawner.spawn(get_aqi(ens160_aqi)).ok();
     spawner.spawn(get_measurements(bme280)).ok();
-    spawner.spawn(blink(led)).ok();
+    spawner.spawn(blink(led, 1000)).ok();
     
     // Create a custom config with a flush callback
     let backend_config = EmbeddedBackendConfig 
     {
-        font_regular: fonts::MONO_6X12_OPTIMIZED,
-        
+        font_regular: fonts::MONO_6X12_OPTIMIZED,        
         ..Default::default()        
     };
 
 
     let backend = EmbeddedBackend::new(&mut display, backend_config);
-
     let mut terminal = Terminal::new(backend).unwrap();
 
-      info!("mousefood set up");
+    info!("mousefood set up");
+
+
 
 
     loop {
-        //info!("Hello world!");
-        //Timer::after(Duration::from_secs(1)).await;
-
+    
         let aqidata = AQISIGNAL.wait().await;
-
         info!("got data: temp {}, hum {}, press {}, tvoc {}, aqi {}", aqidata.temperature, aqidata.humidity, aqidata.pressure, aqidata.tvoc, aqidata.aqi);
-
         
         terminal.draw(
         |frame| {
                 draw(frame, aqidata);
         }).unwrap();    
-
   
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples
 }
 
+const CINFO: Color = Color::Rgb(76, 209, 224);
+const CWARNING: Color = Color::Rgb(209, 154, 102);
+//const BKGD: Color = Color::Rgb(35, 39, 46);
 
-
- const TEAL: Color = Color::Rgb(100, 220, 220);
-const ORANGE: Color = Color::Rgb(250, 145, 55);
 
 fn draw(frame: &mut Frame, aqidata: AirQualityData) {
-        
+
+   
+
     let vertical = Layout::vertical([
         //Constraint::Percentage(30),         
         Constraint::Percentage(30), 
@@ -349,23 +328,23 @@ fn draw(frame: &mut Frame, aqidata: AirQualityData) {
 
     let gauge = match aqidata.aqi {
         1 => Gauge::default()            
-            .gauge_style(Style::new().fg(TEAL).bg(Color::Black))
+            .gauge_style(Style::new().fg(CINFO).bg(Color::Black))
             .ratio(1_f64)
             .label("excellent"), 
         2 => Gauge::default()            
-            .gauge_style(Style::new().fg(TEAL).bg(Color::Black))
+            .gauge_style(Style::new().fg(CINFO).bg(Color::Black))
             .ratio(1_f64)
             .label("good"), 
         3 => Gauge::default()            
-            .gauge_style(Style::new().fg(Color::Black).bg(TEAL))
+            .gauge_style(Style::new().fg(Color::Black).bg(CINFO))
             .ratio(1_f64)
             .label("moderate"), 
         4 => Gauge::default()            
-            .gauge_style(Style::new().fg(Color::Black).bg(ORANGE))
+            .gauge_style(Style::new().fg(Color::Black).bg(CWARNING))
             .ratio(1_f64)
             .label("poor"), 
         5 => Gauge::default()            
-            .gauge_style(Style::new().fg(ORANGE).bg(Color::Black))
+            .gauge_style(Style::new().fg(CWARNING).bg(Color::Black))
             .ratio(1_f64)
             .label("unhealthy"), 
         _ => Gauge::default()            
@@ -376,11 +355,10 @@ fn draw(frame: &mut Frame, aqidata: AirQualityData) {
     };    
 
     let bordered_block = Block::bordered()
-        .border_style(Style::new().fg(TEAL))                
+        .border_style(Style::new().fg(CINFO))                
         .title("Air Quality");
     
     frame.render_widget(gauge.block(bordered_block), second);
-     
 
     // four frames - top left        
 
@@ -388,12 +366,12 @@ fn draw(frame: &mut Frame, aqidata: AirQualityData) {
     write!(&mut textbuffer, "{} C", round_float(aqidata.temperature)).unwrap();
 
     //let paragraph = Paragraph::new(textbuffer.as_str().white())
-    let paragraph = Paragraph::new(textbuffer.as_str().fg(ORANGE))
+    let paragraph = Paragraph::new(textbuffer.as_str().fg(CWARNING))
         .wrap(Wrap { trim: true })
         .centered();
 
     let bordered_block = Block::bordered()
-        .border_style(Style::new().fg(TEAL))
+        .border_style(Style::new().fg(CINFO))
         //.padding(Padding::new(0, 0, third_bottom_left.height / 4, 0))
         .title("Temperature");
     
@@ -404,13 +382,13 @@ fn draw(frame: &mut Frame, aqidata: AirQualityData) {
     let mut textbuffer = ArrayString::<16>::new();
     write!(&mut textbuffer, "{} %", round_float(aqidata.humidity)).unwrap();
 
-    let paragraph = Paragraph::new(textbuffer.as_str().fg(ORANGE))
+    let paragraph = Paragraph::new(textbuffer.as_str().fg(CWARNING))
         .wrap(Wrap { trim: true })
         .centered()
         ;
 
     let bordered_block = Block::bordered()
-        .border_style(Style::new().fg(TEAL))
+        .border_style(Style::new().fg(CINFO))
         //.padding(Padding::new(0, 0, third_bottom_right.height / 4, 0))
         .title("Humidity");
 
@@ -423,13 +401,13 @@ fn draw(frame: &mut Frame, aqidata: AirQualityData) {
     write!(&mut textbuffer, "{} hPa", round_float(aqidata.pressure)).unwrap();
 
 
-    let paragraph = Paragraph::new(textbuffer.as_str().fg(ORANGE))
+    let paragraph = Paragraph::new(textbuffer.as_str().fg(CWARNING))
         .wrap(Wrap { trim: true })
         .centered()
         ;
 
     let bordered_block = Block::bordered()
-        .border_style(Style::new().fg(TEAL))
+        .border_style(Style::new().fg(CINFO))
         //.padding(Padding::new(0, 0, fourth_bottom_left.height / 4, 0))
         .title("Pressure");
 
@@ -441,25 +419,18 @@ fn draw(frame: &mut Frame, aqidata: AirQualityData) {
     let mut textbuffer = ArrayString::<16>::new();
     write!(&mut textbuffer, "{}", aqidata.tvoc).unwrap();
 
-    
-
-    let paragraph = Paragraph::new(textbuffer.as_str().fg(ORANGE))
+    let paragraph = Paragraph::new(textbuffer.as_str().fg(CWARNING))
         .wrap(Wrap { trim: true })
         .centered()
         ;
 
     let bordered_block = Block::bordered()
-        .border_style(Style::new().fg(TEAL))
+        .border_style(Style::new().fg(CINFO))
         //.padding(Padding::new(0, 0, fourth_bottom_right.height / 4, 0))
         .title("TVOC");
 
     frame.render_widget(paragraph.block(bordered_block), fourth_bottom_right);
 
-}
-
-
-fn round_float(val: f32) -> f32 {
-    (((val * 10_f32) as i32) as f32) / 10_f32     
 }
 
 
@@ -473,9 +444,7 @@ async fn get_aqi(mut sensor: Ens160<SharedI2cDevice>) {
 
     loop {    
         if let Ok(status) = sensor.status().await {
-            if status.data_is_ready() {                    
-                // some static data
-                //let envi = Enviro {temperature: 25.00, humidity: 55.00, pressure: 980.0 };
+            if status.data_is_ready() {                                    
                 let envi = sub_bme.next_message_pure().await;
                 let airquality = AirQualityData {
                     tvoc: sensor.tvoc().await.unwrap(),                    
@@ -494,10 +463,11 @@ async fn get_aqi(mut sensor: Ens160<SharedI2cDevice>) {
 
 
 #[embassy_executor::task]
-async fn blink(mut led: Output<'static>) {  
+// blinkenlight
+async fn blink(mut led: Output<'static>, ms: u16) {  
     loop {
         led.toggle();
-        Timer::after(Duration::from_millis(500)).await;
+        Timer::after(Duration::from_millis(ms as u64)).await;
     }
 }
 
@@ -511,12 +481,14 @@ async fn get_measurements(bme: &'static mut AsyncBme280<I2cDevice<'static, NoopR
 
         let measurements = bme.read_sample().await.unwrap();
 
-        //let measurements = sensor.measure(&mut delayns).await.unwrap();
+        
+        /*
         info!("task - Got BME measurements! T: {}°C, RH: {}%, P: {} Pa",             
             measurements.temperature,
             measurements.humidity,
             measurements.pressure
         );
+         */
 
         let envdata = Enviro {
             temperature: measurements.temperature.unwrap_or(0.0),
@@ -528,4 +500,9 @@ async fn get_measurements(bme: &'static mut AsyncBme280<I2cDevice<'static, NoopR
         
         Timer::after(Duration::from_secs(1)).await;
     }
+}
+
+
+fn round_float(val: f32) -> f32 {
+    (((val * 10_f32) as i32) as f32) / 10_f32     
 }
